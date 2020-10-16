eb294f9c478d0fdf0b00e615b93089bce6ff40e9
skarthikeyan0118 <72861375+skarthikeyan0118@users.noreply.github.com>
2020-10-16 14:56:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "nginxsecurity" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["77.4.92.144/32"]
     }
     egress {
         from_port = 0
